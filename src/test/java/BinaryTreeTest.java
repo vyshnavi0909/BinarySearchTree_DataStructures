4d5dcbe9a40ae6e4bc90928a9dc54f6f0352d914
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-05-06 15:36:04 +0530

    ADD: created given BST and added test case to check size

--- src/test/java/BinaryTreeTest.java
+++ src/test/java/BinaryTreeTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class BinaryTreeTest {
     @Test
-    public void given3Nums_WhenAddedToBinaryTree_shouldReturnSizeThree() {
+    public void given3Numbers_WhenAddedToBinaryTree_ShouldReturnSizeThree() {
         NewBinaryTree<Integer> newBinaryTree = new NewBinaryTree<>();
 
         newBinaryTree.add(56);
@@ -13,5 +13,27 @@ public class BinaryTreeTest {
         int size = newBinaryTree.getSize();
 
         Assertions.assertEquals(3,size);
+    }
+
+    @Test
+    public  void givenNumbers_WhenAddredToBinaryTree_ShouldReturnSize(){
+        NewBinaryTree<Integer> newBinaryTree = new NewBinaryTree<>();
+
+        newBinaryTree.add(56);
+        newBinaryTree.add(30);
+        newBinaryTree.add(70);
+        newBinaryTree.add(22);
+        newBinaryTree.add(40);
+        newBinaryTree.add(60);
+        newBinaryTree.add(95);
+        newBinaryTree.add(11);
+        newBinaryTree.add(65);
+        newBinaryTree.add(3);
+        newBinaryTree.add(16);
+        newBinaryTree.add(63);
+        newBinaryTree.add(67);
+
+        int size = newBinaryTree.getSize();
+        Assertions.assertEquals(13, size);
     }
 }
